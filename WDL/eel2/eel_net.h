justinfrankel/licecap

commit a873d5a9f4c5c9611d4bf70d761427451ad4f0bc

Various 64-bit and modern compiler warning fixes, mostly non-functional changes

WDL/jnetlib/netinc.h
@@ -14,11 +14,14 @@
 #include <windows.h>
 #include <stdio.h>
 #include <time.h>
-#define strcasecmp(x,y) stricmp(x,y)
 #define ERRNO (WSAGetLastError())
 #define SET_SOCK_BLOCK(s,block) { unsigned long __i=block?0:1; ioctlsocket(s,FIONBIO,&__i); }
-#define EWOULDBLOCK WSAEWOULDBLOCK
-#define EINPROGRESS WSAEWOULDBLOCK
+#ifndef EWOULDBLOCK
+  #define EWOULDBLOCK WSAEWOULDBLOCK
+#endif
+#ifndef EINPROGRESS
+  #define EINPROGRESS WSAEWOULDBLOCK
+#endif
 typedef int socklen_t;
 
 #else
@@ -58,10 +61,6 @@ typedef int SOCKET;
 #endif
 #ifndef strnicmp
 #define strnicmp(x,y,z) strncasecmp(x,y,z)
-#endif
-
-#ifndef wsprintf
-#define wsprintf sprintf
 #endif
 
 #endif // !_WIN32

WDL/circbuf.h
@@ -73,7 +73,7 @@ public:
     int put = l;
     int l2;
     if (!m_size) return 0;
-    l2 = m_endbuf - m_head;
+    l2 = (int) (m_endbuf - m_head);
     if (l2 <= l) 
     {
       memcpy(m_head, p, l2);
@@ -102,7 +102,7 @@ public:
     got = l;
     if (m_tail+l >= m_endbuf) 
     {
-      int l1 = m_endbuf - m_tail;
+      int l1 = (int) (m_endbuf - m_tail);
       l -= l1;
       memcpy(p, m_tail, l1);
       m_tail = (char *)m_hb.Get();

WDL/dirscan.h
@@ -68,14 +68,14 @@ class WDL_DirScan
 #endif
      ) // returns 0 if success
     {
-      int l=strlen(dirname);
+      size_t l=strlen(dirname);
       if (l < 1) return -1;
 
       WDL_String scanstr(dirname);
 #ifdef _WIN32
       if (!isExactSpec) 
 #endif
-	 if (dirname[l-1] == '\\' || dirname[l-1] == '/')  scanstr.SetLen(l-1);
+	 if (dirname[l-1] == '\\' || dirname[l-1] == '/')  scanstr.SetLen((int)l-1);
    
    m_leading_path.Set(scanstr.Get());
 

WDL/jnetlib/asyncdns.cpp
@@ -124,7 +124,7 @@ int JNL_AsyncDNS::resolve(const char *hostname, unsigned int *addr)
 
   for (x = 0; x < m_cache_size; x ++)
   {
-    if (!strcasecmp(m_cache[x].hostname,hostname) && m_cache[x].mode==0)
+    if (!stricmp(m_cache[x].hostname,hostname) && m_cache[x].mode==0)
     {
       m_cache[x].last_used=time(NULL);
       if (m_cache[x].resolved)

WDL/jnetlib/connection.cpp
@@ -166,7 +166,13 @@ void JNL_Connection::run(int max_send_bytes, int max_recv_bytes, int *bytes_sent
         FD_SET(m_socket,&f[2]);
         struct timeval tv;
         memset(&tv,0,sizeof(tv));
-        if (select(m_socket+1,&f[0],&f[1],&f[2],&tv)==-1)
+        if (select(
+#ifdef _WIN32
+          0
+#else
+          m_socket+1
+#endif
+          ,&f[0],&f[1],&f[2],&tv)==-1)
         {
           m_errorstr="connecting to host (calling select())";
           m_state=STATE_ERROR;
@@ -350,7 +356,7 @@ int JNL_Connection::send(const void *_data, int length)
 
 int JNL_Connection::send_string(const char *line)
 {
-  return send(line,strlen(line));
+  return send(line,(int)strlen(line));
 }
 
 int JNL_Connection::recv_bytes_available(void)

WDL/jnetlib/httpget.cpp
@@ -180,14 +180,14 @@ void JNL_HTTPGet::connect(const char *url, int ver, const char *requestmethod)
 
   if (!m_http_proxyhost || !m_http_proxyhost[0])
   {
-    wsprintf(str,"%s %s HTTP/1.%d\r\n",requestmethod,m_http_request,ver%10);
+    sprintf(str,"%s %s HTTP/1.%d\r\n",requestmethod,m_http_request,ver%10);
   }
   else
   {
-    wsprintf(str,"%s %s HTTP/1.%d\r\n",requestmethod, m_http_url,ver%10);
+    sprintf(str,"%s %s HTTP/1.%d\r\n",requestmethod, m_http_url,ver%10);
   }
 
-  wsprintf(str+strlen(str),"Host:%s\r\n",m_http_host);
+  sprintf(str+strlen(str),"Host:%s\r\n",m_http_host);
 
   if (m_http_lpinfo&&m_http_lpinfo[0])
   {

WDL/lameencdec.cpp
@@ -883,7 +883,7 @@ LameEncoder::~LameEncoder()
     if (m_vbrfile.Get()[0] && lame_get_lametag_frame)
     {
       unsigned char buf[16384];
-      int a=lame_get_lametag_frame(m_lamestate,buf,sizeof(buf));
+      size_t a=lame_get_lametag_frame(m_lamestate,buf,sizeof(buf));
       if (a>0 && a<=sizeof(buf))
       {
         FILE *fp=NULL;

WDL/lineparse.h
@@ -290,10 +290,10 @@ class LineParser
       return 0;
     }
 
-    char * WDL_LINEPARSE_PREFIX tmpbufalloc(int sz)
+    char * WDL_LINEPARSE_PREFIX tmpbufalloc(size_t sz)
     {
       if (sz<1)sz=1;
-      if (sz+m_tmpbuf_used <= (int)sizeof(m_tmpbuf))
+      if (sz+m_tmpbuf_used <= sizeof(m_tmpbuf))
       {
          m_tmpbuf_used+=sz;
          return m_tmpbuf + m_tmpbuf_used - sz;
@@ -315,15 +315,15 @@ class LineParser
 #ifdef WDL_LINEPARSE_INTF_ONLY
     void freetokens();
     int doline(const char *line, int ignore_escaping);
-    char *tmpbufalloc(int sz);
+    char *tmpbufalloc(size_t sz);
     void tmpbuffree(char *p);
 #endif
 
+    size_t m_tmpbuf_used;
+    char **m_tokens;
     int m_eat;
     int m_nt;
-    int m_tmpbuf_used;
     bool m_bCommentBlock;
-    char **m_tokens;
     char m_tmpbuf[2048];
 };
 #endif//!WDL_LINEPARSE_IMPL_ONLY

cfillion/WDL

commit fa01a8329ac9a56aff9d59991e219145f512fc7f

jnetlib: fix client connections when building with VS2017+
EINPROGRESS (112) is already defined by errno.h and doesn't get set to
WSAEWOULDBLOCK (10035) as required by JNetLib.

https://forum.cockos.com/showthread.php?t=195247
https://forum.cockos.com/showthread.php?p=2107550

(Issue introduced in a873d5a9f4c5c9611d4bf70d761427451ad4f0bc)

WDL/eel2/eel_net.h
@@ -34,9 +34,10 @@
     #define INVALID_SOCKET (-1)
   #endif
 #else
-  #ifndef ENOTCONN
-    #define ENOTCONN WSAENOTCONN
+  #ifdef ENOTCONN
+    #undef ENOTCONN
   #endif
+  #define ENOTCONN WSAENOTCONN
 #endif
 
 class eel_net_state

WDL/jnetlib/netinc.h
@@ -14,14 +14,20 @@
 #include <windows.h>
 #include <stdio.h>
 #include <time.h>
+
 #define ERRNO (WSAGetLastError())
 #define SET_SOCK_BLOCK(s,block) { unsigned long __i=block?0:1; ioctlsocket(s,FIONBIO,&__i); }
-#ifndef EWOULDBLOCK
-  #define EWOULDBLOCK WSAEWOULDBLOCK
+
+#ifdef EWOULDBLOCK
+  #undef EWOULDBLOCK
 #endif
-#ifndef EINPROGRESS
-  #define EINPROGRESS WSAEWOULDBLOCK
+#define EWOULDBLOCK WSAEWOULDBLOCK
+
+#ifdef EINPROGRESS
+  #undef EINPROGRESS
 #endif
+#define EINPROGRESS WSAEWOULDBLOCK
+
 typedef int socklen_t;
 
 #else